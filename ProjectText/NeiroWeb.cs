76a0a001419ecfd8d8ef47b9dddc9d7e00a6a0a4
kirichenkokirill70 <kirich70@icloud.com>
2017-06-12 18:02:16 +0700

    Add files via upload

---
+++ ProjectText/NeiroWeb.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace ProjectText
+{
+
+    class NeiroWeb
+    {
+        
+        public  const int          neironInArrayWidth  =           10; 
+        public  const int          neironInArrayHeight =           10; 
+        private const string       memory              = "memory.txt"; 
+        private       List<Neiron> neironArray         =         null; 
+
+        public NeiroWeb()
+        {
+            neironArray = InitWeb();            
+        }
+
+        private static List<Neiron> InitWeb()
+        {
+            if (!File.Exists(memory)) return new List<Neiron>();
+            string[] lines = File.ReadAllLines(memory);
+            if (lines.Length == 0)    return new List<Neiron>();
+            string jStr = lines[0];
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            List<Object> objects = json.Deserialize<List<Object>>(jStr);
+            List<Neiron> res = new List<Neiron>();
+            foreach (var o in objects) res.Add(NeironCreate((Dictionary<string,Object>)o));
+            return res;
+        }
+
+        private static Neiron NeironCreate(Dictionary<string, object> o)
+        {
+            Neiron res = new Neiron();
+            res.name = (string)o["name"];
+            res.countTrainig = (int)o["countTrainig"];
+            Object[] veightData = (Object[])o["veight"];
+            int arrSize = (int)Math.Sqrt(veightData.Length);
+            res.veight = new double[arrSize, arrSize];
+            int index = 0;
+            for (int n = 0; n < res.veight.GetLength(0); n++)
+                for (int m = 0; m < res.veight.GetLength(1); m++)
+                {
+                    res.veight[n, m] = Double.Parse(veightData[index].ToString());
+                    index++;
+                }
+            return res;
+        }
+
+
+        public string CheckLitera(int[,] arr)
+        {
+            string res = null;
+            double max = 0;
+            foreach (var n in neironArray)
+            {
+                double d = n.GetRes(arr);
+                if (d > max)
+                {
+                    max = d;
+                    res = n.GetName();
+                }
+            }
+            return res;
+        }
+
+        public void SaveState()
+        {
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            string jStr = json.Serialize(neironArray);
+            System.IO.StreamWriter file = new System.IO.StreamWriter(memory);
+            file.WriteLine(jStr);
+            file.Close();
+        }      
+
+        public string[] GetLiteras()
+        {
+            var res = new List<string>();
+            for (int i = 0; i < neironArray.Count; i++) res.Add(neironArray[i].GetName());
+            res.Sort();
+            return res.ToArray();
+        }
+
+        
+        public void SetTraining(string trainingName, int[,] data)
+        {
+            Neiron neiron = neironArray.Find(v => v.name.Equals(trainingName));
+            if (neiron == null) 
+            {                   
+                neiron = new Neiron();
+                neiron.Clear(trainingName, neironInArrayWidth, neironInArrayHeight);
+                neironArray.Add(neiron);
+            }
+            int countTrainig = neiron.Training(data); 
+            string messageStr = "Имя образа - " + neiron.GetName() + 
+                                " вариантов образа в памяти - " + countTrainig.ToString();
+
+            
+            Form resultForm = new ShowMemoryVeight(neiron);
+            resultForm.Text = messageStr;
+            resultForm.Show();
+        }
+
+       
+    }
+}
